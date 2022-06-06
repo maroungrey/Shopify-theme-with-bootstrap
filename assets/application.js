83622a6be574c21f29c48ce1719bcd192cc19a6c
Maroun <62679040+maroungrey@users.noreply.github.com>
2022-06-05 18:37:33 -0700

    Add files via upload

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,9 @@
-// Put your application javascript here
+// Creates functionality for filters in the Collections page
+if( document.getElementById('sort_by') != null) {
+    document.querySelector('#sort_by').addEventListener('change', function(e) {
+        var url = new URL(window.location.href);
+        url.searchParams.set('sort_by', e.currentTarget.value);
+
+        window.location = url.href;
+    });
+}
